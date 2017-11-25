d53549cd2a6ea9ebbc6658e3523d6b815111430a
Liam Maddison <liam@signable.co.uk>
2017-11-25 16:38:36 +0200

    Resource routes

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ $router->get('/', function () use ($router) {
 $router->post('/login', 'Auth\LoginProxy@attemptLogin');
 
 /**
- * Users Resource routes
+ * User Resource routes
  */
 $router->group(['prefix' => 'users', 'middleware' => 'auth'], function () use ($router) {
     $router->get('/', 'UsersController@all');
@@ -29,4 +29,37 @@ $router->group(['prefix' => 'users', 'middleware' => 'auth'], function () use ($
     $router->put('/update/{id}', 'UsersController@update');
     $router->post('/create', 'UsersController@create');
     $router->delete('/delete/{id}', 'UsersController@delete');
+});
+
+/**
+ * Shop Resource routes
+ */
+$router->group(['prefix' => 'shops', 'middleware' => 'auth'], function () use ($router) {
+    $router->get('/', 'ShopsController@all');
+    $router->get('/{id}', 'ShopsController@getById');
+    $router->put('/update/{id}', 'ShopsController@update');
+    $router->post('/create', 'ShopsController@create');
+    $router->delete('/delete/{id}', 'ShopsController@delete');
+});
+
+/**
+ * Product Resource routes
+ */
+$router->group(['prefix' => 'products', 'middleware' => 'auth'], function () use ($router) {
+    $router->get('/', 'ProductsController@all');
+    $router->get('/{id}', 'ProductsController@getById');
+    $router->put('/update/{id}', 'ProductsController@update');
+    $router->post('/create', 'ProductsController@create');
+    $router->delete('/delete/{id}', 'ProductsController@delete');
+});
+
+/**
+ * Cart Resource routes
+ */
+$router->group(['prefix' => 'carts', 'middleware' => 'auth'], function () use ($router) {
+    $router->get('/', 'CartsController@all');
+    $router->get('/{id}', 'CartsController@getById');
+    $router->put('/update/{id}', 'CartsController@update');
+    $router->post('/create', 'CartsController@create');
+    $router->delete('/delete/{id}', 'CartsController@delete');
 });
