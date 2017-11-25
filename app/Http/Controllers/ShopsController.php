f938c92bf015b8572113b53a42bfb69dda6e3490
Liam Maddison <liam@signable.co.uk>
2017-11-25 16:36:31 +0200

    Controllers

---
+++ app/Http/Controllers/ShopsController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class ShopsController extends RESTActions
+{
+    /**
+     * Specify Repository class name
+     *
+     * @return mixed
+     */
+    public function repository()
+    {
+        return 'App\Repositories\ShopRepository';
+    }
+}
